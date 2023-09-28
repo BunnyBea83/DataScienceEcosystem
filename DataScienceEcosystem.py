93792f84f7dfa293fb41ade2f2bc003a16afe67a
Bea <146021916+BunnyBea83@users.noreply.github.com>
2023-09-27 17:30:15 -0700

    Upload my Jupyter Notebook
    Uploaded Jupyter notebook that follows the coursera test prompts

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives:**
+# + List popular languages for Data Science
+# + List commonly used Libraries for Daat Scientists
+# + Give examples of simple arithmatic
+# 
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. Java
+# 3. HTML
+# 4. C++
+# 5. Ruby
+# 6. Scala
+# 
+# 
+
+# ### Some commonly used libraries used by Data Scientists include:
+# 1. Numpy
+# 2. Pandas
+# 3. Matplotlib
+# 4. Seaborn
+# 5. Scikit-learn
+
+# | Data Science Tools|
+# |-------------------|
+# | Jupyter Notebooks|
+# | Apache Zeppelin|
+# | R Studio|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# ## Author
+# Bea Sauve
+
+# In[ ]:
+
+
+
+
